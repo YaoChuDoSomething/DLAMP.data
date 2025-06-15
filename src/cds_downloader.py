44f3e7784f8936effcc35a5dfd79da7db046d6b6
YaoChuWu <yaochuwu.tw@gmail.com>
2025-06-15 19:16:11 +0800

    Update cds_downloader.py

--- src/cds_downloader.py
+++ src/cds_downloader.py
@@ -1,32 +1,40 @@
 import yaml
 import cdsapi
 import xarray as xr
-import numpy as np
 from datetime import datetime, timedelta
 from tqdm import tqdm
-from cdo import Cdo
 import os
 
-class ERA5DataLoader:
+"""
+share:
+  time_control:
+
+download:
+  file_io:
+  area:
+  variables:
+
+regrid:
+diagnostics:
+"""
+
+class CDSDataDownloader:
     def __init__(self, yaml_path):
         self.cfg = self._load_config(yaml_path)
 
-        self.start_t = datetime.strptime(
-            self.cfg['setup_time']['start'], 
-            self.cfg['setup_time']['format']
-        )
-        self.end_t = datetime.strptime(
-            self.cfg['setup_time']['end'], 
-            self.cfg['setup_time']['format']
-        )
-        self.a_timestep = timedelta(
-            hours=self.cfg['setup_time']['base_step_hours']
-        )
+        self.cfg_time = self.cfg["share"]["time_control"]
+        self.start_t = datetime.strptime(self.cfg_time["start"], self.cfg_time["format"])
+        self.end_t = datetime.strptime(self.cfg_time["end"], self.cfg_time["format"])
+        self.a_timestep = timedelta(hours=self.cfg_time["base_step_hours"])
         self.total_steps = ((self.end_t - self.start_t) // self.a_timestep) + 1
 
-        self.grib_dir = self.cfg['output']['grib']
-        self.netcdf_dir = self.cfg['output']['netcdf']
-        self.timestr_fmt = self.cfg['output']['prefix']['timestr_fmt']
+        self.cfg_io = self.cfg["share"]["file_io"]
+        self.grib_dir = self.cfg_io['grib_dir']
+        self.netcdf_dir = self.cfg_io['netcdf_dir']
+        self.prefix = self.cfg_io["prefix"]
+        self.pl_prefix = self.prefix["pres_lev"]
+        self.sl_prefix = self.prefix["sing_lev"]
+        self.timestr_fmt = self.prefix['timestr_fmt']
         os.makedirs(self.grib_dir, exist_ok=True)
         os.makedirs(self.netcdf_dir, exist_ok=True)
 
@@ -38,8 +46,6 @@ class ERA5DataLoader:
         ]
 
         self.client = cdsapi.Client()
-        self.cdo = Cdo(tempdir="./cdo_tmp")
-        self.cdo.debug = True
 
     def _load_config(self, yaml_path):
         with open(yaml_path, "r") as f:
@@ -61,22 +67,23 @@ class ERA5DataLoader:
             req["area"] = self.area_list
         return req
 
-    def invert_grids_in_lat(self, input_file: str, output_file: str):
-        with xr.open_dataset(input_file, engine="netcdf4") as ds:
-            lat_dim_name = None
-            for dim in ['latitude', 'lat', 'y']:
-                if dim in ds.dims:
-                    lat_dim_name = dim
+    def convert_grib_to_netcdf(self, grib_file: str, netcdf_file: str):
+        """
+        使用 xarray 讀取 GRIB 並轉為 NetCDF，並反轉緯度順序（南到北）
+        """
+        try:
+            ds = xr.open_dataset(grib_file, engine="cfgrib")
+
+            # 嘗試找到緯度維度並反轉
+            for lat_name in ["latitude", "lat"]:
+                if lat_name in ds.dims:
+                    ds = ds.sortby(lat_name, ascending=True)
                     break
 
-            if lat_dim_name is None:
-                raise ValueError("No latitude dimension found.")
-
-            ds[lat_dim_name] = ds[lat_dim_name][::-1]
-            for var_name, da in ds.data_vars.items():
-                if lat_dim_name in da.dims:
-                    ds[var_name] = da.isel({lat_dim_name: slice(None, None, -1)})
-            ds.to_netcdf(output_file, format='NETCDF4')
+            ds.to_netcdf(netcdf_file, format="NETCDF4")
+            ds.close()
+        except Exception as e:
+            print(f"[ERROR] GRIB 轉換失敗: {grib_file}\n{e}")
 
     def process_download(self):
         for i in tqdm(range(self.total_steps), desc="Downloading ERA5", unit="step"):
@@ -88,25 +95,20 @@ class ERA5DataLoader:
             sl_grb = os.path.join(self.grib_dir, f"{self.cfg['output']['prefix']['sing_lev']}_{timestamp}.grib")
             sl_nc = os.path.join(self.netcdf_dir, f"{self.cfg['output']['prefix']['sing_lev']}_{timestamp}.nc")
 
+            # 壓力層資料
             if "dataset_upper" in self.cfg:
                 p = self.cfg["dataset_upper"]
                 req = self._build_request(p['title'], p['variables'], curr_time, p.get('levels'))
                 self.client.retrieve(p['title'], req).download(pl_grb)
-                self.cdo.invertlat(
-                    input=self.cdo.copy(input=pl_grb, options="-f nc4 --eccodes"),
-                    options="-f nc4",
-                    output=pl_nc
-                )
+                self.convert_grib_to_netcdf(pl_grb, pl_nc)
 
+            # 單層資料
             if "dataset_surface" in self.cfg:
                 s = self.cfg["dataset_surface"]
                 req = self._build_request(s['title'], s['variables'], curr_time)
                 self.client.retrieve(s['title'], req).download(sl_grb)
-                self.cdo.invertlat(
-                    input=self.cdo.copy(input=sl_grb, options="-f nc4 --eccodes"),
-                    options="-f nc4",
-                    output=sl_nc
-                )
+                self.convert_grib_to_netcdf(sl_grb, sl_nc)
+
 
 if __name__ == "__main__":
     config_file = "../config/era5.yaml"
