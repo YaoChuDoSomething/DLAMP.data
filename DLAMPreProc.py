ecdf1700bd0192cd83ed4840171aa5015926455f
YaoChuWu <yaochuwu.tw@gmail.com>
2025-06-17 17:51:12 +0800

    Update DLAMPreProc.py

--- DLAMPreProc.py
+++ DLAMPreProc.py
@@ -1,12 +1,29 @@
-import yaml
-import numpy as np
-import xarray as xr
-from src.cds_downloader import ERA5DataLoader
+#!/bin/python
 
-## ERA5 Dataset Downloading ###
-era5_config = "./config/era5.yaml"
-downloader = ERA5DataLoader(era5_config)
+DLAMP_DATA_DIR = "./"
+
+###===== ERA5 Dataset Downloading ===================================###
+#   Downloading process is fully controled by YAML configure file
+###==================================================================###
+
+from src.cds_downloader import CDSDataDownloader
+
+era5_config = f"{DLAMP_DATA_DIR}/config/era5.yaml"
+downloader = CDSDataDownloader(era5_config)
 downloader.process_download()
 
-### ###
+
+###===== DataRegridder and Variables Registry =======================###
+#   Data Regridding can be controled by YAML configure file
+#   Variables Registry can be controled by YAML configure file
+#   Variables Diagnostics can be controled by module script:
+#       src/registry/diagnostics_functions
+###==================================================================###
+
+from src.diag_variables import DataRegridder
+
+yaml_file = f"{DLAMP_DATA_DIR}/config/DLAMPreproc.yaml"
+regridder = DataRegridder(yaml_file)
+regridder.main_process()
+
 
