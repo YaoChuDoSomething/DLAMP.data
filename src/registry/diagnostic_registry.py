bebb955d869b5104073c663a4eecfdd5d597931a
YaoChuWu <yaochuwu.tw@gmail.com>
2025-06-17 17:45:36 +0800

    Create diagnostic_registry.py

---
+++ src/registry/diagnostic_registry.py
@@ -0,0 +1,40 @@
+import importlib
+import yaml
+
+def load_diagnostics(yaml_path):
+    with open(yaml_path, "r") as f:
+        cfg = yaml.safe_load(f)
+    
+    reg_cfg = cfg.get("registry", {})
+    diagnostics = {}
+
+    for name, item in reg_cfg.items():
+        func_name = item["function"]
+        module = importlib.import_module("src.registry.diagnostic_functions")
+        func = getattr(module, func_name)
+        diagnostics[name] = {
+            "requires": item["requires"],
+            "function": func,
+            "long_name": item.get("long_name", ""),
+            "units": item.get("units", "")
+        }
+    return diagnostics
+
+def sort_diagnostics_by_dependencies(diagnostics):
+    sorted_list = []
+    visited = set()
+
+    def visit(var):
+        if var in visited:
+            return
+        for dep in diagnostics[var]["requires"]:
+            if dep in diagnostics:
+                visit(dep)
+        sorted_list.append(var)
+        visited.add(var)
+
+    for var in diagnostics:
+        visit(var)
+
+    return sorted_list
+
